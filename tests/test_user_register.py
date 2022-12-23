eb22d1977a468565b20d2923d02c4a07fcc1670d
AvaOleh <avakimyanov@gmail.com>
2022-12-23 17:56:12 +0300

    refactor class TestUserRegister

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -7,20 +7,9 @@ from lib.base_case import BaseCase
 
 
 class TestUserRegister(BaseCase):
-    def setup_method(self):
-        base_part = "learnqa"
-        domain = "example.com"
-        ramdom_part = datetime.now().strftime("%m%d%Y%H%M%S")
-        self.email = f"{base_part}{ramdom_part}@{domain}"
 
     def test_create_user_successfully(self):
-        data = {
-            'password': '123',
-            'username': 'learnqa',
-            'firstName': 'learnqa',
-            'lastName': 'learnqa',
-            'email': self.email
-        }
+        data = self.prepare_registration_data()
 
         response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
 
@@ -29,13 +18,7 @@ class TestUserRegister(BaseCase):
 
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
-        data = {
-            'password': '123',
-            'username': 'learnqa',
-            'firstName': 'learnqa',
-            'lastName': 'learnqa',
-            'email': email
-        }
+        data = self.prepare_registration_data(email)
 
         response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
 
