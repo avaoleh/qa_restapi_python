c05295caf943483d496abea389d97c9ff3858998
AvaOleh <avakimyanov@gmail.com>
2022-12-23 19:08:29 +0300

    refactor all test_user_{...}.py

--- lib/my_requests.py
+++ lib/my_requests.py
@@ -20,7 +20,7 @@ class MyRequests():
     @staticmethod
     def _send(url: str, data: dict, headers: dict, cookies: dict, method: str):
 
-        url = f"https://playground.learnqa.ru/api"
+        url = f"https://playground.learnqa.ru/api{url}"
 
         if headers is None:
             headers = {}
@@ -30,11 +30,11 @@ class MyRequests():
         if method == 'GET':
             response = requests.get(url, params=data, headers=headers, cookies=cookies)
         elif method == 'POST':
-            response = requests.get(url, data=data, headers=headers, cookies=cookies)
+            response = requests.post(url, data=data, headers=headers, cookies=cookies)
         elif method == 'PUT':
-            response = requests.get(url, data=data, headers=headers, cookies=cookies)
+            response = requests.put(url, data=data, headers=headers, cookies=cookies)
         elif method == 'DELETE':
-            response = requests.get(url, data=data, headers=headers, cookies=cookies)
+            response = requests.delete(url, data=data, headers=headers, cookies=cookies)
         else:
             raise Exception(f"Bad HTTP method '{method}' was received")
 
--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -1,9 +1,7 @@
 import pytest
-import requests
-
 from lib.assertions import Assertions
 from lib.base_case import BaseCase
-
+from lib.my_requests import MyRequests
 
 class TestUserAuth(BaseCase):
     exclude_params = [
@@ -17,7 +15,9 @@ class TestUserAuth(BaseCase):
             'password': '1234'
         }
 
-        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+        #response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+
+        response1 = MyRequests.post("/user/login", data=data)
 
         self.auth_sid = self.get_cookie(response1, "auth_sid")
         self.token = self.get_header(response1, "x-csrf-token")
@@ -25,8 +25,8 @@ class TestUserAuth(BaseCase):
 
     def test_auth_user(self):
 
-        response2 = requests.get(
-            "https://playground.learnqa.ru/api/user/auth",
+        response2 = MyRequests.get(
+            "/user/auth",
             headers={"x-csrf-token": self.token},
             cookies={"auth_sid": self.auth_sid}
         )
@@ -42,13 +42,13 @@ class TestUserAuth(BaseCase):
     def test_negative_auth_check(self, condition):
 
         if condition == "no_cookie":
-            response2 = requests.get(
-                "https://playground.learnqa.ru/api/user/auth",
+            response2 = MyRequests.get(
+                "/user/auth",
                 headers={"x-csrf-token": self.token}
             )
         else:
-            response2 = requests.get(
-                "https://playground.learnqa.ru/api/user/auth",
+            response2 = MyRequests.get(
+                "/user/auth",
                 cookies={"auth_sid": self.auth_sid}
             )
 
